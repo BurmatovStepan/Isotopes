8017aa9fd5180cc6f0937b9d1285156e22fcbe46
BurmatovStepan <nohy@windows.localdomain>
2026-09-07 17:13:34 +0300

    docs: clean main branch

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { IsotopesModule } from './isotopes/isotopes.module.js';
-
-@Module({
-    imports: [IsotopesModule],
-})
-export class AppModule { }
